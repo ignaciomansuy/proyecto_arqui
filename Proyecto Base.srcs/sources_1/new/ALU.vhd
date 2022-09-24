afa12f5b4c94b83fdcdc655751e2b9e88c75828c
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-09-24 16:59:42 -0300

    Initial commit

---
+++ Proyecto Base.srcs/sources_1/new/ALU.vhd
@@ -0,0 +1,88 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ALU is
+    Port ( a        : in  std_logic_vector (7 downto 0);   -- Primer operando.
+           b        : in  std_logic_vector (7 downto 0);   -- Segundo operando.
+           sop      : in  std_logic_vector (2 downto 0);   -- Selector de la operación.
+           c        : out std_logic;                       -- Señal de 'carry'.
+           z        : out std_logic;                       -- Señal de 'zero'.
+           n        : out std_logic;                       -- Señal de 'nagative'.
+           result   : out std_logic_vector (7 downto 0));  -- Resultado de la operación.
+end ALU;
+
+architecture Behavioral of ALU is
+
+component Adder8 is
+    Port ( a  : in  std_logic_vector (7 downto 0);
+           b  : in  std_logic_vector (7 downto 0);
+           ci : in  std_logic;
+           s  : out std_logic_vector (7 downto 0);
+           co : out std_logic);
+end component;
+
+signal alu_result   : std_logic_vector(7 downto 0);
+signal adder_result   : std_logic_vector(7 downto 0);
+signal ci   : std_logic;
+signal co   : std_logic;
+signal adder_b   : std_logic_vector(7 downto 0);
+
+
+begin
+
+-- Sumador/Restaror
+
+ci <= sop(0) and not sop(1) and not sop(2);
+
+with ci select
+    adder_b <= b when '0',
+               not b when others;
+
+inst_Adder8: Adder8 port map(
+        a      =>a,
+        b      =>adder_b,
+        ci     =>ci,
+        s      =>adder_result,
+        co     =>co
+    );
+    
+
+
+                
+-- Resultado de la Operación
+               
+with sop select
+    alu_result <= adder_result     when "000",
+                  adder_result     when "001",
+                  a and b          when "010",
+                  a or b           when "011",
+                  a xor b          when "100",
+                  not a            when "101",
+                  '0' & a(7 downto 1)  when "110",
+                  a(6 downto 0) & '0'  when "111",
+                  "00000000" when others;
+                  
+result  <= alu_result;
+
+-- Flags c z n
+with sop select
+    c <= co     when "000",
+         co     when "001",
+         '0'     when "010",
+         '0'     when "011",
+         '0'     when "100",
+         '0'     when "101",
+         a(0)     when "110",
+         a(7)     when "111",
+         '0' when others;
+            
+with alu_result select
+    z <= '1' when "00000000",
+        '0' when others;
+     
+with sop select
+    n <= not co when "001",
+        '0' when others;          
+    
+end Behavioral;
+
---
+++ Proyecto Base.srcs/sources_1/new/Basys3.vhd
@@ -0,0 +1,167 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Basys3 is
+    Port (
+        sw          : in   std_logic_vector (15 downto 0); -- No Tocar - Señales de entrada de los interruptores -- Arriba   = '1'   -- Los 16 swiches.
+        btn         : in   std_logic_vector (4 downto 0);  -- No Tocar - Señales de entrada de los botones       -- Apretado = '1'   -- 0 central, 1 arriba, 2 izquierda, 3 derecha y 4 abajo.
+        led         : out  std_logic_vector (15 downto 0); -- No Tocar - Señales de salida  a  los leds          -- Prendido = '1'   -- Los 16 leds.
+        clk         : in   std_logic;                      -- No Tocar - Señal de entrada del clock              -- Frecuencia = 100Mhz.
+        seg         : out  std_logic_vector (7 downto 0);  -- No Tocar - Salida de las señales de segmentos.
+        an          : out  std_logic_vector (3 downto 0)   -- No Tocar - Salida del selector de diplay.
+          );
+end Basys3;
+
+architecture Behavioral of Basys3 is
+
+-- Inicio de la declaración de los componentes.
+    
+component Debouncer -- No Tocar
+    Port (
+        clk         : in    std_logic;
+        signal_in   : in    std_logic;
+        signal_out  : out   std_logic
+          );
+    end component;
+    
+component Display_Controller -- No Tocar
+    Port (  
+        dis_a       : in    std_logic_vector (3 downto 0);
+        dis_b       : in    std_logic_vector (3 downto 0);
+        dis_c       : in    std_logic_vector (3 downto 0);
+        dis_d       : in    std_logic_vector (3 downto 0);
+        clk         : in    std_logic;
+        seg         : out   std_logic_vector (7 downto 0);
+        an          : out   std_logic_vector (3 downto 0)
+          );
+    end component;
+    
+component Reg -- No Tocar
+    Port (
+        clock       : in    std_logic;
+        clear       : in    std_logic;
+        load        : in    std_logic;
+        up          : in    std_logic;
+        down        : in    std_logic;
+        datain      : in    std_logic_vector (7 downto 0);
+        dataout     : out   std_logic_vector (7 downto 0)
+          );
+    end component;
+
+component ALU -- No Tocar
+    Port ( 
+        a           : in    std_logic_vector (7 downto 0);
+        b           : in    std_logic_vector (7 downto 0);
+        sop         : in    std_logic_vector (2 downto 0);
+        c           : out   std_logic;
+        z           : out   std_logic;
+        n           : out   std_logic;
+        result      : out   std_logic_vector (7 downto 0)
+          );
+    end component;
+
+-- Fin de la declaración de los componentes.
+
+-- Inicio de la declaración de señales.
+
+signal d_btn            : std_logic_vector(4 downto 0);  -- Señales de botones con anti-rebote.
+            
+signal dis_a            : std_logic_vector(3 downto 0);  -- Señales de salida al display A.    
+signal dis_b            : std_logic_vector(3 downto 0);  -- Señales de salida al display B.     
+signal dis_c            : std_logic_vector(3 downto 0);  -- Señales de salida al display C.    
+signal dis_d            : std_logic_vector(3 downto 0);  -- Señales de salida al display D.
+     
+signal a                : std_logic_vector(7 downto 0);  -- Señales del primer operador.    
+signal b                : std_logic_vector(7 downto 0);  -- Señales del segundo operador.  
+
+signal result           : std_logic_vector(7 downto 0);  -- Señales del resultado.
+
+signal datain           : std_logic_vector(7 downto 0);  -- Señales de datos de entrada a los registros.
+
+
+-- Fin de la declaración de señales.
+
+begin
+
+-- Inicio de declaración de comportamientos.
+
+-- Muxer Regs
+with sw(12) select
+    datain <= result            when '0',
+              sw(7 downto 0)    when others;
+
+-- Muxers del Display
+with btn(0) select
+    dis_a <= a(7 downto 4)      when '0',
+             "0000"             when others;
+                     
+with btn(0) select
+    dis_b <= a(3 downto 0)      when '0',
+             "0000"             when others;
+
+with btn(0) select
+    dis_c <= b(7 downto 4)      when '0',
+             result(7 downto 4) when others;
+                
+with btn(0) select
+    dis_d <= b(3 downto 0)      when '0',
+             result(3 downto 0) when others;
+
+
+-- Inicio de declaración de instancias.
+
+
+
+inst_REG_A: Reg port map( -- Repárame!
+    clock       => d_btn(2),
+    clear       => '0',
+    load        => '1',
+    up          => '0',
+    down        => '0',
+    datain      => datain,
+    dataout     => a
+    );
+    
+inst_REG_B: Reg port map( -- Repárame!
+    clock       => d_btn(3),
+    clear       => '0',
+    load        => '1',
+    up          => '0',
+    down        => '0',
+    datain      => datain,
+    dataout     => b
+    );
+ 
+ inst_ALU: ALU port map(
+    a           => a,
+    b           => b,
+    sop         => sw(15 downto 13),
+    c           => led(15),
+    z           => led(14),
+    n           => led(13),
+    result      => result
+    );
+
+-- Intancia de Display_Controller.        
+inst_Display_Controller: Display_Controller port map(
+    dis_a => dis_a, 
+    dis_b => dis_b, 
+    dis_c => dis_c, 
+    dis_d => dis_d,     
+    clk => clk ,        -- No Tocar - Entrada del clock completo (100Mhz).
+    seg => seg,         -- No Tocar - Salida de las señales de segmentos. 
+    an => an            -- No Tocar - Salida del selector de diplay.
+    );
+
+-- No Tocar - Intancias de Debouncers.    
+inst_Debouncer0: Debouncer port map( clk => clk, signal_in => btn(0), signal_out => d_btn(0) );
+inst_Debouncer1: Debouncer port map( clk => clk, signal_in => btn(1), signal_out => d_btn(1) );
+inst_Debouncer2: Debouncer port map( clk => clk, signal_in => btn(2), signal_out => d_btn(2) );
+inst_Debouncer3: Debouncer port map( clk => clk, signal_in => btn(3), signal_out => d_btn(3) );
+inst_Debouncer4: Debouncer port map( clk => clk, signal_in => btn(4), signal_out => d_btn(4) );
+    
+-- Fin de declaración de instancias.
+
+-- Fin de declaración de comportamientos.
+  
+end Behavioral;
