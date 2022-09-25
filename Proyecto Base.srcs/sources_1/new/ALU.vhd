60ffed469711d978c2697f66a4f39de794c08b26
emilioizzo6 <emilioizzo@uc.cl>
2022-09-25 17:19:45 -0400

    modify ALU and Adder8 to create Adder16

--- Proyecto Base.srcs/sources_1/new/ALU.vhd
+++ Proyecto Base.srcs/sources_1/new/ALU.vhd
@@ -2,30 +2,30 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity ALU is
-    Port ( a        : in  std_logic_vector (7 downto 0);   -- Primer operando.
-           b        : in  std_logic_vector (7 downto 0);   -- Segundo operando.
+    Port ( a        : in  std_logic_vector (15 downto 0);   -- Primer operando.
+           b        : in  std_logic_vector (15 downto 0);   -- Segundo operando.
            sop      : in  std_logic_vector (2 downto 0);   -- Selector de la operación.
            c        : out std_logic;                       -- Señal de 'carry'.
            z        : out std_logic;                       -- Señal de 'zero'.
            n        : out std_logic;                       -- Señal de 'nagative'.
-           result   : out std_logic_vector (7 downto 0));  -- Resultado de la operación.
+           result   : out std_logic_vector (15 downto 0));  -- Resultado de la operación.
 end ALU;
 
 architecture Behavioral of ALU is
 
-component Adder8 is
-    Port ( a  : in  std_logic_vector (7 downto 0);
-           b  : in  std_logic_vector (7 downto 0);
+component Adder16 is
+    Port ( a  : in  std_logic_vector (15 downto 0);
+           b  : in  std_logic_vector (15 downto 0);
            ci : in  std_logic;
-           s  : out std_logic_vector (7 downto 0);
+           s  : out std_logic_vector (15 downto 0);
            co : out std_logic);
 end component;
 
-signal alu_result   : std_logic_vector(7 downto 0);
-signal adder_result   : std_logic_vector(7 downto 0);
+signal alu_result   : std_logic_vector(15 downto 0);
+signal adder_result   : std_logic_vector(15 downto 0);
 signal ci   : std_logic;
 signal co   : std_logic;
-signal adder_b   : std_logic_vector(7 downto 0);
+signal adder_b   : std_logic_vector(15 downto 0);
 
 
 begin
@@ -38,7 +38,7 @@ with ci select
     adder_b <= b when '0',
                not b when others;
 
-inst_Adder8: Adder8 port map(
+inst_Adder16: Adder16 port map(
         a      =>a,
         b      =>adder_b,
         ci     =>ci,
@@ -58,9 +58,9 @@ with sop select
                   a or b           when "011",
                   a xor b          when "100",
                   not a            when "101",
-                  '0' & a(7 downto 1)  when "110",
-                  a(6 downto 0) & '0'  when "111",
-                  "00000000" when others;
+                  '0' & a(15 downto 1)  when "110",
+                  a(14 downto 0) & '0'  when "111",
+                  "0000000000000000" when others;
                   
 result  <= alu_result;
 
@@ -73,11 +73,11 @@ with sop select
          '0'     when "100",
          '0'     when "101",
          a(0)     when "110",
-         a(7)     when "111",
+         a(15)     when "111",
          '0' when others;
             
 with alu_result select
-    z <= '1' when "00000000",
+    z <= '1' when "0000000000000000",
         '0' when others;
      
 with sop select
